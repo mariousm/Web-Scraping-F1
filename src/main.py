d199aa7d04ca17e6389346dcfa0bc967f872d709
mariousm <mariousm7@gmail.com>
2021-03-27 21:35:40 +0100

    Parseo del html y guardado del valor año y enlace

---
+++ src/main.py
@@ -0,0 +1,8 @@
+# IMPORTS
+from scraper import Scraper
+
+# Creamos el scraper y lo ejectuamos
+scraper = Scraper()
+scraper.__scraping__()
+
+# Guardamos a un fichero CSV
--- src/scraper.py
+++ src/scraper.py
@@ -1,6 +1,39 @@
+# IMPORTS
+import cloudscraper
+from bs4 import BeautifulSoup
 
+# Clase encarga de extraer los datos
 class Scraper():
 
+    # Constructor de la clase
     def __init__(self):
         self.url = "https://www.f1-fansite.com/f1-results/"
         self.data = []
+        self.soup = None
+
+    # Método que parsea el HTML a un objeto BeautifulSoup con el que navegar el DOM
+    # https://stackoverflow.com/questions/49087990/python-request-being-blocked-by-cloudflare
+    def __getHtml__(self, url):
+        scraper = cloudscraper.create_scraper()
+        soup = BeautifulSoup(scraper.get(url).text)
+        return soup
+
+    # Obtenemos una lista de listas con el año y su enlance correspondiente dentro de la página
+    # [[año, "link"]]
+    def __getYearsLinks__(self, html):
+        lstYearsLinks = []
+
+        for link in html.find_all('a'):
+            lstYearsLinks.append([int(link.text), link['href']])
+
+        return lstYearsLinks
+
+    # Método main para realizar el web-scraping
+    def __scraping__(self):
+        lstYearsLinks = []
+        self.soup = self.__getHtml__(self.url)
+        
+        # Obtenemos los años juntos con sus enlaces
+        eTable = self.soup.find('table') #Obtenemos la primera tabla en la cual se encuentran los años por temporada
+        lstYearsLinks = self.__getYearsLinks__(eTable)
+        print(lstYearsLinks)
